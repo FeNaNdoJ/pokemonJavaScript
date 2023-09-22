46bcff228f149ffff2361cd8d31ca402cc70e2c7
Fernando Junio <fernandojunior8576@gmail.com>
2023-09-22 10:56:29 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('botao')
+    .addEventListener('click', function () {
+nome =document.getElementById('nome-pokemon').value
+
+
+
+
+        fetch('https://pokeapi.co/api/v2/pokemon/'+nome)
+            .then(function (response) {
+                return response.json()
+            }).then(function (response) {
+                console.log(response.sprites.front_default)
+
+               console.log(response.abilities)
+
+               let html_habilidades = ''
+               for (const hab of response.abilities) {
+                html_habilidades += '<li>'+hab.ability.name+'</li>'
+                
+                console.log(hab.ability.name)
+               }
+               let lista = document.getElementById ('habilidades')
+               lista.innerHTML = html_habilidades
+
+
+                let url = response.sprites.front_default
+                document.getElementById('img-card').setAttribute('src', url)
+            })
+
+    })    
